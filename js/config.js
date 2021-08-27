5c2aaaa69064ee039613f8da22308f4803e89727
toshiki515 <atoshiki515@icloud.com>
2021-08-27 22:36:34 +0900

    first commit

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+	auth0: {
+		domain: 'dev-kqoqmt5g.us.auth0.com',
+		clientId: 'AYdfo5XjyeMtxczJiSzjEL8wrYzC32gc'
+	},
+  apiBaseUrl: 'https://pnr3lfkpvf.execute-api.us-east-1.amazonaws.com/dev'
+};
