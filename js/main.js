c6abd3dbafaadb710473e1688a15ea4b64796848
toshiki515 <atoshiki515@icloud.com>
2021-08-31 22:41:34 +0900

    asdfjadf

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-/**
- * Created by Peter Sbarski
- * Serverless Architectures on AWS
- * http://book.acloud.guru/
- * Last Updated: Feb 11, 2017
- */
-
-
- (function(){
+  
+(function(){
 	$(document).ready(function(){
 		userController.init(configConstants);
+		videoController.init(configConstants);
 	});
 }());
